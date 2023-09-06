db0ab3f9f2a84e7c2399365bf56da0a7b10e0c5f
habibullo <azimovhabibullo1@gmail.com>
2023-09-06 21:54:06 +0500

    post url added in polls.urls

--- app1/urls.py
+++ app1/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import All, Detail, All2, Detail2
+from .views import All, Detail, All2, Detail2, CreateModelView, CreateOfficeView
 
 urlpatterns = [
     path('detail/<int:my_id>', Detail.as_view()),
     path('all/', All.as_view()),
     path('detail2/<int:my_office__id>', Detail2.as_view()),
     path('all2/', All2.as_view()),
+    path('create1/', CreateModelView.as_view()),
+    path('create2/', CreateOfficeView.as_view()),
 ]
